c3af9919434f0ecadb46da0ad6f8a2dc5c2febab
Edi <ungureanu.eduard2001@gmail.com>
2023-09-20 12:04:39 +0300

    Added UI.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,7 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.Json;
 
 import java.util.ArrayList;
@@ -16,12 +23,18 @@ public class MyGdxGame extends ApplicationAdapter {
 	private ArrayList<Level> levels;
 	private int currentLevel;
 	private Texture backgroundTexture;
+	private Stage stage;
+
+	private boolean inMenu = true; // Indicates if the game is in the menu
+
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		player = new PlayerCharacter();
 		levels = new ArrayList<>();
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage); // Set the stage as the input processor
 
 		// Load levels from the JSON file
 		Json json = new Json();
@@ -39,73 +52,150 @@ public class MyGdxGame extends ApplicationAdapter {
 		currentLevel = 0; // Start with the first level
 		loadLevel(currentLevel);
 
-		// Play song for the first level
+		// Play song for the first level / menu
 		if (currentLevel == 0) {
 			Level currentLevelObject = levels.get(currentLevel);
 			currentLevelObject.getLevelMusic().play();
 		}
+
+		// Load button images for the menu
+		Texture replayTexture = new Texture("UI/PlayButton.png");
+		Texture optionTexture = new Texture("UI/OptionsButton.png");
+		Texture exitTexture = new Texture("UI/ExitButton.png");
+		Texture boxTexture = new Texture("UI/Box.png");
+		Texture bannerTexture = new Texture("UI/Banner.png");
+
+		// Create button drawables
+		Drawable replayDrawable = new SpriteDrawable(new Sprite(replayTexture));
+		Drawable OptionDrawable = new SpriteDrawable(new Sprite(optionTexture));
+		Drawable exitDrawable = new SpriteDrawable(new Sprite(exitTexture));
+		Drawable boxDrawable = new SpriteDrawable(new Sprite(boxTexture));
+		Drawable bannerDrawable = new SpriteDrawable(new Sprite(bannerTexture));
+
+		// Create Replay button
+		ImageButton replayButton = new ImageButton(replayDrawable);
+		ImageButton optionButton = new ImageButton(OptionDrawable);
+		ImageButton exitButton = new ImageButton(exitDrawable);
+		Image boxImage = new Image(boxDrawable);
+		Image bannerImage = new Image(bannerDrawable);
+
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+
+		float centerX = (screenWidth - 250) / 2;
+		float centerY = (screenHeight - 80) / 2;
+
+		replayButton.setPosition(centerX, centerY + 50);
+		optionButton.setPosition(centerX, centerY - 33);
+		exitButton.setPosition(centerX, centerY - 117);
+		boxImage.setPosition(centerX-75, centerY-150);
+		bannerImage.setPosition(centerX-5, centerY+158);
+
+
+		// Add buttons to the stage
+		stage.addActor(boxImage);
+		stage.addActor(replayButton);
+		stage.addActor(optionButton);
+		stage.addActor(exitButton);
+		stage.addActor(bannerImage);
+
+
+		// Add click listeners to buttons
+		replayButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, com.badlogic.gdx.scenes.scene2d.Actor actor) {
+				// Start the game with the first level
+				inMenu = false;
+				loadLevel(0);
+			}
+		});
+
+		exitButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, com.badlogic.gdx.scenes.scene2d.Actor actor) {
+				// Exit the game
+				Gdx.app.exit();
+			}
+		});
+
+		// Set up the background texture
+		backgroundTexture = new Texture("UI/background.jpg");
 	}
 
 	@Override
 	public void render() {
 		float deltaTime = Gdx.graphics.getDeltaTime();
 
-		// Handle user input and update the character
-		player.update(deltaTime, levels.get(currentLevel).getPlatforms());
+		if (inMenu) {
+			// Clear the screen
+			Gdx.gl.glClearColor(0, 0, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// Check if the character's x-coordinate has reached the nextLevelCoordinate of the current level
-		if (player.getPosition().x >= levels.get(currentLevel).getNextLevelCoordinate().x) {
-			// Increment the currentLevel and load the next level
-			currentLevel++;
-			if (currentLevel < levels.size()) {
-				Level currentLevelObject = levels.get(currentLevel);
-				loadLevel(currentLevel);
-				currentLevelObject.getLevelMusic().play();
-				// Reset the character's position or perform other level transition logic if needed
-				player.setPosition(1, 60);
-			} else {
-				// Handle the case where there are no more levels
-				Gdx.app.exit();
-				return;
+			batch.begin();
+			batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.end();
+
+			// Draw the menu buttons
+			stage.act();
+			stage.draw();
+		} else {
+			// Handle user input and update the character
+			player.update(deltaTime, levels.get(currentLevel).getPlatforms());
+
+			// Check if the character's x-coordinate has reached the nextLevelCoordinate of the current level
+			if (player.getPosition().x >= levels.get(currentLevel).getNextLevelCoordinate().x) {
+				// Increment the currentLevel and load the next level
+				currentLevel++;
+				if (currentLevel < levels.size()) {
+					levels.get(currentLevel - 1).getLevelMusic().stop();
+					Level currentLevelObject = levels.get(currentLevel);
+					loadLevel(currentLevel);
+					currentLevelObject.getLevelMusic().play();
+					// Reset the character's position or perform other level transition logic if needed
+					player.setPosition(1, 60);
+				} else {
+					// Handle the case where there are no more levels
+					Gdx.app.exit();
+					return;
+				}
 			}
+
+			// Clear the screen
+			Gdx.gl.glClearColor(0, 0, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+			batch.begin();
+
+			// Get the screen dimensions
+			float screenWidth = Gdx.graphics.getWidth();
+			float screenHeight = Gdx.graphics.getHeight();
+
+			// Scale and render the background image to fit the screen
+			batch.draw(backgroundTexture, 0, 0, screenWidth, screenHeight);
+
+			// Render platforms
+			for (Platform platform : levels.get(currentLevel).getPlatforms()) {
+				platform.render(batch);
+			}
+
+			// Render spikes
+			for (Platform spike : levels.get(currentLevel).getSpikes()) {
+				spike.render(batch);
+			}
+
+			// Render the character
+			player.render(batch);
+
+			batch.end();
 		}
-
-		// Clear the screen
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-
-		// Get the screen dimensions
-		float screenWidth = Gdx.graphics.getWidth();
-		float screenHeight = Gdx.graphics.getHeight();
-
-		// Scale and render the background image to fit the screen
-		batch.draw(backgroundTexture, 0, 0, screenWidth, screenHeight);
-
-		// Render platforms
-		for (Platform platform : levels.get(currentLevel).getPlatforms()) {
-			platform.render(batch);
-		}
-
-		// Render spikes
-		for (Platform spike : levels.get(currentLevel).getSpikes()) {
-			spike.render(batch);
-		}
-
-		// Render the character
-		player.render(batch);
-
-		batch.end();
 	}
-
-
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		player.dispose();
-		backgroundTexture.dispose(); // Dispose of the backgroundTexture
+		backgroundTexture.dispose();
+		stage.dispose();
 	}
 
 	public void loadLevel(int levelIndex) {
@@ -129,4 +219,3 @@ public class MyGdxGame extends ApplicationAdapter {
 		return Gdx.input.isKeyPressed(Input.Keys.SPACE);
 	}
 }
-
